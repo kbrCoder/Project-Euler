e38a6a42318d13ed9722c190f9637da2beff8b7c
Sessiz <143322363+sessizkup@users.noreply.github.com>
2024-10-11 15:14:09 +0300

    1.Soru.cs

--- 001/1.Soru.cs
+++ 001/1.Soru.cs
@@ -1,8 +1,3 @@
-/*Eğer $10$'un altındaki tüm doğal sayıları listelersek ve bu sayılardan
- * $3$ veya $5$'in katları olanları seçersek, bu sayılar
- * $3, 5, 6$ ve $9$ olur. Bu katların toplamı $23$'tür.
-$1000$'ın altındaki tüm $3$ veya $5$ katlarının toplamını bulun.*/
-
 int toplam = 0;
 for(int i=1;i<1000;i++)
 {
